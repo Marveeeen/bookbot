ec231d68818127935be1de6ba22226177a685dd7
Marveeeen <jmvroqueee@gmail.com>
2025-07-06 18:08:27 +0800

    feat: refactor solution

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import sys
 from stats import *
 
-
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        return file_contents
-
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
-    file_path = f"./{sys.argv[1]}"
-    book_text =get_book_text(file_path)
-    num_words = get_num_words(book_text)
-    num_word_dict =  get_num_word_dict(book_text)
-    character_list = get_list_of_characters(num_word_dict)
-    sorted_character_list = sort_character_list(character_list)
+    book = sys.argv[1]
+    book_text = get_book_text(book)
+    char_list = get_char_list(book_text)
+
+    # Printed results
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
+    print(f"Found {get_num_words(book_text)} total words")
     print("--------- Character Count -------")
-    for character in sorted_character_list:
-        print(f"{character['char']}: {character['num']}")
+    for char in char_list:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
+
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,33 +1,27 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
 def get_num_words(text):
-    text = text.split()
-    return len(text)
+    return len(text.split())
 
-def get_num_word_dict(text):
-    text = text.lower().split()
-    text = "".join(text)
-    text_dict = {}
-
-    for word in text:
-        if word in text_dict:
-            text_dict[word] += 1
-        else:
-            text_dict[word] = 1
-    
-    return text_dict
-
-def get_list_of_characters(text_dict):
-    character_list = []
-    for word, count in text_dict.items():
-        character_list.append({"char": word, "num": count})
-        
-
-    return character_list
+def get_char_dict(text):
+    text_set = set(text.lower())
+    return dict((char, text.count(char)) for char in text_set)
 
 def sort_on(items):
     return items["num"]
 
-def sort_character_list(text_dict):
+def sort_char_list(text_dict):
     text_dict.sort(reverse=True, key=sort_on)
     return text_dict
+
+def get_char_list(book_text):
+    text_dict = get_char_dict(book_text)
+    char_list = [{"char": char, "num": num} for char, num in text_dict.items() if char.isalpha()]
+
+    return sort_char_list(char_list)
+
 
     
